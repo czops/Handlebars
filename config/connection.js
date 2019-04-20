d7733bb2e1535de46fce00e23a313d1e69c01e92
czops <37029225+czops@users.noreply.github.com>
2019-04-20 12:49:03 -0500

    Added some more to the structure of this

---
+++ config/connection.js
@@ -0,0 +1,24 @@
+//this needs to have the correct database and variable names
+
+// Set up MySQL connection.
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "Mi/amanqar0",
+  database: "cat_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
+module.exports = connection;
